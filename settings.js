5860e4b307456f96ff67245ce0b75bdd5de931e3
luxalgo2025 <pathummalsara16@gmail.com>
2025-08-29 23:08:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,3 +9,4 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'KSMD~2oQ0GJQI#FsUeb8W2XqDNmP
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
+};
