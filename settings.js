7e3ac8d19925cd6d4d3b201014bfecef69952be9
Mr-Unknownz <mrsandeshbhashana@gmail.com>
2025-08-30 14:42:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,10 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'KSMD~64gAUKpL#L54BesgPi6N_495aAQtfaIushlYADTHNzuGzz3Wd5S0' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || '.' ,
+PREFIX: process.env.PREFIX || '.',
+FOOTER: process.env.FOOTER || '> *© Powered By Nova-X Md 💸*',
+BOT_NAME: process.env.BOT_NAME || '𝐍ｏ𝐕𝐀-ｘ Ｍ𝐃',
+OWNER_NAME: process.env.OWNER_NAME || 'ᴘᴀᴛʜᴜᴍ ᴍᴀʟꜱᴀʀᴀ & ꜱᴀɴᴅᴇꜱʜ ʙʜᴀʜᴀɴᴀ',
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
 };
