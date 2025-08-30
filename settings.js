34337dda44c38f059207684dc018792b1ed37b0c
Mr-Unknownz <mrsandeshbhashana@gmail.com>
2025-08-30 19:13:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,6 +9,7 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'KSMD~64gAUKpL#L54BesgPi6N_49
 PREFIX: process.env.PREFIX || '.',
 FOOTER: process.env.FOOTER || '> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴏᴠᴀ-x-ᴍᴅ 💸*',
 BOT_NAME: process.env.BOT_NAME || '𝐍ｏ𝐕𝐀-ｘ Ｍ𝐃',
+BUTTON: process.env.BUTTON || 'true',
 OWNER_NAME: process.env.OWNER_NAME || '_Mr Unknown X Luxalgo_',
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
