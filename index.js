99547bfda3dd123d99e430df974626a552fff2e1
luxalgo2025 <pathummalsara16@gmail.com>
2025-09-10 23:01:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -193,22 +193,12 @@ conn.ev.on('creds.update', saveCreds)
 
 //================== AUTO STATUS VIEW ==================
 
-mek.message = (getContentType(mek.message) === 'ephemeralMessage') 
-    ? mek.message.ephemeralMessage.message 
-    : mek.message
-
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true") {
-    await conn.readMessages([mek.key])  
-    const mnyako = await jidNormalizedUser(conn.user.id)
-
-    // ✅ React optional check
-    if (config.AUTO_STATUS_REACT && config.AUTO_STATUS_REACT !== "true") {
-        await conn.sendMessage(
-            mek.key.remoteJid,
-            { react: { key: mek.key, text: config.AUTO_STATUS_IMOJI } },
-            { statusJidList: [mek.key.participant, mnyako] }
-        )
-    }
+if (!mek.message) return	
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])  
+const mnyako = await jidNormalizedUser(conn.user.id)
+await conn.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '❤️'}}, { statusJidList: [mek.key.participant, mnyako] })
 }
 
 if (mek.key && mek.key.remoteJid === 'status@broadcast') return
