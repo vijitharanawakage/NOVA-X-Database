c45c288783df00c3c825e5262152b15874a8c0c8
Mr-Unknownz <mrsandeshbhashana@gmail.com>
2025-09-10 23:30:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,6 +9,7 @@ SESSION_ID: process.env.SESSION_ID || 'KSMD~OhYFkQCR#_hcEbAzzBTnUrrlDjBkZd7qeQdg
 PREFIX: process.env.PREFIX || '.',
 ALIVE_IMG: process.env.ALIVE_IMG || 'https://files.catbox.moe/er0vnl.png',   
 FOOTER: process.env.FOOTER || '> *©卩ᴏᴡᴇʀᴇᴅ ʙʏ ɴᴏᴠᴀ-x ᴍᴅ 👾*',
+ALIVE_MSG: process.env.ALIVE_MSG || '👾 Ｗ𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 𝐍ｏ𝐕𝐀-ｘ Ｍ𝐃',
 BOT_NAME: process.env.BOT_NAME || '𝐍ｏ𝐕𝐀-ｘ Ｍ𝐃',
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
 BUTTON: process.env.BUTTON || 'true',
