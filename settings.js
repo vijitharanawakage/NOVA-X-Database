3b0910852c3f8d7b279f2a4e413ed0c2e3052929
luxalgo2025 <pathummalsara16@gmail.com>
2025-09-10 20:11:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,7 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID || 'KSMD~OhYFkQCR#_hcEbAzzBTnUrrlDjBkZd7qeQdg8TaOxVHi5H9igZew',
 PREFIX: process.env.PREFIX || '.',
+ALIVE_IMG: process.env.ALIVE_IMG || 'true',   
 FOOTER: process.env.FOOTER || '> *©卩ᴏᴡᴇʀᴇᴅ ʙʏ ɴᴏᴠᴀ-x ᴍᴅ 👾*',
 BOT_NAME: process.env.BOT_NAME || '𝐍ｏ𝐕𝐀-ｘ Ｍ𝐃',
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
@@ -18,3 +19,4 @@ OWNER_NAME: process.env.OWNER_NAME || '_Mr Unknown X Luxalgo_',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94723975388',
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
 };
+ALIVE_IMG
